ef3287f87051fb6219b251b56c7788e4fe69061f
lightprime04-max <lightprime04@gmail.com>
2026-03-31 17:52:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
   res.send("Backend working 🚀");
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log("Server running");
 });
